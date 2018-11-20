5ebacefb792e7eae0e658a6e0de070e3f6290b17
Ekrem Guzelyel <ekremguzelyel@Ekrems-MacBook-Pro.local>
2018-11-20 12:40:39 -0600

    Reseting Git directory

--- edu/Models/labeled_functions.py
+++ edu/Models/labeled_functions.py
@@ -59,3 +59,33 @@ def split_and_vectorize():
    print("Data Vectorized")
    
    return X_train_vector , y_train, X_test_vector , y_test
+
+def split_ngram(m,n):
+   # Load from path
+   path=r"/Users/ekremguzelyel/Desktop/Assignments/Research/MLLab-IIT/edu/models/"
+   X_corpus , y = load_labeled(path)
+   X_train_corpus , X_test_corpus, y_train, y_test = train_test_split(X_corpus, y, test_size=1./3, random_state=42)
+    
+   # Vectorize the data with ngram
+   token = r"(?u)\b[\w\'/]+\b"
+   vectorizer = CountVectorizer(token_pattern=token, min_df=5, stop_words=["the","a","of","and","br","to"], ngram_range=(m,n))
+
+   X_train_vector = vectorizer.fit_transform(X_train_corpus)
+   X_test_vector = vectorizer.transform(X_test_corpus)
+   print("Data Vectorized with ngram")
+   
+   return X_train_vector , y_train, X_test_vector , y_test
+
+def convert_to_sequence(vector):
+    ''' Takes a vector and converts it into a sequence.
+    Args:
+        vector: An output of CountVectorizer. i.e X_train_vector, X_test_vector
+    '''
+    document_sequence = []
+
+    for k in vector.toarray():
+        sequence = []
+        [sequence.append(i) for i,j in enumerate(k) if j]
+        document_sequence.append(sequence)
+
+    return document_sequence
